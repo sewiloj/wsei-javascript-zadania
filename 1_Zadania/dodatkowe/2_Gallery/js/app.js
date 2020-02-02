ac801c68d343b4e603bbc6742cb878c871347de4
unknown <jakub.sewilo@microsoft.wsei.edu.pl>
2020-02-02 16:19:09 +0100

    Add additional task 2

--- 1_Zadania/dodatkowe/2_Gallery/js/app.js
+++ 1_Zadania/dodatkowe/2_Gallery/js/app.js
@@ -4,3 +4,48 @@
    <button class="close">Close</button>
  </div>
  */
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log('Działa');
+
+    let listContent = document.querySelectorAll('li');
+    let body = document.querySelector('body');
+
+    console.log(body);
+    console.log(listContent);
+
+    listContent.forEach(el => {
+        el.addEventListener('click', function(){
+            console.log('dziala');
+            let src = this.firstChild.src;
+            console.log(this.firstChild.src);
+            fullScreenPicture(src);
+        });
+    });
+
+    function fullScreenPicture(src)
+    {
+        console.log('fullScreen');
+        const html = ` <div class="fullScreen">
+        <img src="${src}">
+        <button class="close">Close</button>
+        </div>`;
+
+      body.innerHTML += html;
+
+      const closeButton = document.querySelector('.close');
+      closeButton.addEventListener('click', function(){
+        removeFullScreen();
+      });
+    }
+
+    function removeFullScreen()
+    {
+        let removeHtml = document.querySelector('.fullScreen');
+        body.removeChild(removeHtml);
+    }
+
+
+    // Problem jest to, że funkcja działa tylko raz
+
+});
